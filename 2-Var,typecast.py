76ed4d828ec430595a80df2ec217e91c58a61664
SJN21 <102046671+SJN21@users.noreply.github.com>
2022-09-13 20:34:51 +0530

    var-typecast.py

--- 2-Var,typecast.py
+++ 2-Var,typecast.py
@@ -1,26 +1,26 @@
-# #variables
-# a=3
-# b=7.3
-# print(a+b)
-# print(a-b)
-# print(type(a))
-# print(type(b))
-# print(type(a+b))
+#variables
+a=3
+b=7.3
+print(a+b)
+print(a-b)
+print(type(a))
+print(type(b))
+print(type(a+b))
 
-# #Typecasting
-# c="34"
-# d="74"
-# print(int(c)+int(d))
+#Typecasting
+c="34"
+d="74"
+print(int(c)+int(d))
 
-# #printing a string n times
-#     #In this 10 will be multiplied by the ans.
-# print(10*(int(c)+int(d)))
+#printing a string n times
+    #In this 10 will be multiplied by the ans.
+print(10*(int(c)+int(d)))
 
-#     #In this 10 will be multiplied by c.
-# print(10*int(c)+int(d))
+    #In this 10 will be multiplied by c.
+print(10*int(c)+int(d))
 
-#     #In this the ans is converted into string and than that is printed 10 times
-# print(10*str(int(c)+int(d)))
+    #In this the ans is converted into string and than that is printed 10 times
+print(10*str(int(c)+int(d)))
 
 a="6"
 b="8"
