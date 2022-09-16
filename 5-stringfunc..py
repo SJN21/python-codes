c025542a62da65d2f2a33143222c83525137b620
SJN21 <102046671+SJN21@users.noreply.github.com>
2022-09-16 09:03:39 +0530

    Add files via upload

---
+++ 5-stringfunc..py
@@ -0,0 +1,12 @@
+var1= "Hi, this is SJN"
+
+#boolean functions
+print(var1.isalnum())
+print(var1.isalpha())
+print(var1.endswith("SJN"))
+print(var1.count("s"))
+print(var1.capitalize())
+print(var1.upper())
+print(var1.lower())
+print(var1.isalnum())
+print(var1.replace("is","are"))
