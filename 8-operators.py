3d9d414917ab4596da8044e4174255c6e35a6c74
SJN21 <102046671+SJN21@users.noreply.github.com>
2022-09-18 16:43:16 +0530

    Add files via upload

---
+++ 8-operators.py
@@ -0,0 +1,7 @@
+''' arithmatic operators[+,-,*,/,%,**->power mate..]
+assignment operator[a=3 -> =,+=]
+comparision operator[==,<,>,!=..]
+Logical operators[and,or,exor,not,..]
+identity operator[is,is not]
+membership operator[in,not in]
+bitwise operator[logical operate kari aapse]'''
