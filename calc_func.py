886b6235b7be3bbb705e1c81fd77d50a312f78f8
SJN21 <102046671+SJN21@users.noreply.github.com>
2022-09-21 10:20:46 +0530

    Add files via upload

---
+++ calc_func.py
@@ -0,0 +1,18 @@
+a=4
+b=3
+m=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(b%a)
+print(a!=b)
+print(a^b) #xor
+print(0 or 1) #if both are 0 than 0
+print(0 and b) #if both are 1 than 1
+print(3**4) #power
+print(a & b) #logical and
+print(a | b) #logical or
+print(pow(a,b)) # power a^b
+print(pow(a,b,m)) #(a^b)%m power modulo m
