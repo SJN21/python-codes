5f9f07e5bafbf84fe4b6947d92a84ad1fd4c05ea
SJN21 <102046671+SJN21@users.noreply.github.com>
2022-09-23 19:06:08 +0530

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,5 @@
+#enumerate che e index pan aapse and e position par je hase iterable eto kharuj etle e index and iterable
+for i,c in enumerate("hi how are you?"):
+    print(i,c)
+for i,c in enumerate([1,73,34,"hau"]):
+    print(i,c)
